cdbc76b0732abe63fd7d67e3ed1bec289ed0bdee
Christos Sapounas <christossapounas@192.168.1.6>
2026-03-31 01:47:49 +0300

    Rust API + PYTHON

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,20 +1,85 @@
 use axum::{Json, Router, response::IntoResponse, routing::get};
 use serde_json::json;
+use std::process::Command;
 
+//HTTP
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/api", get(hello_world));
+async fn main() {    
+    listening(pathing()).await;
+}
 
-    // listen globally on port 3000
+fn pathing()->Router{
+    let app = Router::new()
+        .route("/api", get(test))
+        .route("/ndvi", get(ndvi_anal));
+    app
+}
+
+async fn listening(app: Router){
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Server started successfully at 0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn hello_world() -> impl IntoResponse {
-    let json_response = json!({
-        "status": "ok",
-        "message": "Hello, World!"
+
+async fn ndvi_anal()-> impl IntoResponse{
+    let json_response=json!({
+        "status": "NDVI analyzation beep boop"
     });
+    println!("{}", json_response);
     Json(json_response)
 }
+
+
+//continue testing to call a python file from rust
+async fn test() -> impl IntoResponse {
+    let json_response = json!({
+        "status": "OK",
+        "message": "Hello, World!"
+    });
+    println!("{}", json_response);
+    Json(json_response);
+    //calling python
+    let conda_python = "/home/christossapounas/.conda/envs/odc_env/bin/python3.10";
+    
+    let output = Command::new(conda_python)
+        .arg("/run/media/christossapounas/AEGON/Thesis_Hellas_Cube/Hellas_Cube/script.py")
+        // Pass the conda env's bin to PATH so sub-imports work
+        .env("PATH", "/home/christossapounas/miniforge3/bin:/usr/bin:/bin")
+        .env("CONDA_PREFIX", "/home/christossapounas/.conda/envs/odc_env/bin:/home/christossapounas/miniforge3/condabin:/home/christossapounas/.cargo/bin:/home/christossapounas/.local/bin:/home/christossapounas/bin:/usr/local/bin:/usr/bin:/var/lib/snapd/snap/bin")
+        // Your custom vars
+        .output()
+        .expect("Failed to run Python script");
+
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    eprintln!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    
+}
+
+/*
+HTTPS
+
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use axum_server::tls_rustls::RustlsConfig;
+use serde_json::json;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let config = RustlsConfig::from_pem_file(
+        "cert.pem",  // your certificate
+        "key.pem",   // your private key
+    )
+    .await
+    .unwrap();
+
+    let app = Router::new().route("/api", get(hello_world));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 443));
+    println!("Server started at https://0.0.0.0:443");
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+*/
---
+++ script.py
@@ -0,0 +1,8 @@
+import warnings
+import geopandas as gpd
+from datacube import Datacube
+from odc.geo.geom import Geometry
+from shapely.geometry import box
+warnings.filterwarnings('ignore')
+
+print("WAssupppppp from pythonnnnn")
