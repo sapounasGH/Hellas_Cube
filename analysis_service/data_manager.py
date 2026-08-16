1742e6c995682cfb7c457b19e4f3c4c60a5087cc
sapounasGH <christianosapounas@gmail.com>
2026-08-17 00:49:14 +0300

    ADDED:

--- analysis_service/data_manager.py
+++ analysis_service/data_manager.py
@@ -12,30 +12,35 @@ import rasterio
 
 class data_manager:
 
+    #constructor
     def __init__(self, dc: Datacube, check: check_data):
         self.dc=dc
         self.check=check
         pass
 
+    #loading the dataset only for sentinel2 because sentinel data can activate threading
     def load_s2(self, place: str, date1: str, date2: str, req_type: str, measurements: list,resolution: tuple, product: list):
-    #loading the dataset need to do something diffrent for the resolution
-        odc_geom, desired_dates, datasets = self.check.checking(place, date1, date2, ["sentinel_2_l2a"], req_type)   
+        #Checking for data in the database and making sure they exist, returning important variables for the loading
+        odc_geom, desired_dates, datasets = self.check.checking(place, date1, date2, ["sentinel_2_l2a"], req_type)
+
+        #loading dataset
         meas=list(dict.fromkeys(measurements + ["scl"]))  #adding scl, SCL is a way to mask the pixels
         ds=self.dc.load(
-            product=product,
-            datasets=datasets,
-            geopolygon=odc_geom,
-            time=desired_dates,
-            output_crs=constants.CRS_GREECE,
-            resolution=resolution,
-            measurements=meas,
-            dask_chunks={"time": 1, "x": 1024, "y": 1024},
-            group_by="solar_day",
+            product=product,                                #product ex. sentinel-2-l2a
+            datasets=datasets,                              #datasets found from the checking method
+            geopolygon=odc_geom,                            #geopolygon of the area
+            time=desired_dates,                             #dates
+            output_crs=constants.CRS_GREECE,                #the greek grid 
+            resolution=resolution,                          #resolution
+            measurements=meas,                              #channels, aka colors
+            dask_chunks={"time": 1, "x": 1024, "y": 1024},  #dask chunks fro threads
+            group_by="solar_day",                           #grouping the observations into a single time slice
         )
         return ds
 
+    #method loading dataset with a reasterio envirometn, needs a loader with os enrviroment variables
+    #Before calling this method, set enviroment variables
     def load_dataset_with_env(self, place: str, date1: str, date2: str, req_type: str, measurements: list,resolution: tuple, product: list):
-    #loading the dataset need to do something diffrent for the resolution
         odc_geom, desired_dates, datasets = self.check.checking(place, date1, date2, product, req_type)   
         with rasterio.Env():
             ds=self.dc.load(
@@ -50,11 +55,12 @@ class data_manager:
             )
         return ds
 
+    #prefixing the stats 
     def prefix_stats(d: dict, prefix: str) -> dict:
         return {f"{prefix}_{k}": v for k, v in d.items()}
-    
+
+    #returning "pretty" statistics from the dataset
     def stats(self, da: Datacube, index_name: str = "") -> dict:
-        #returning 
         valid_px=int(da.notnull().sum().values)
         total_px=int(da.size)
         coverage=round((valid_px / total_px) * 100, 2) if total_px > 0 else 0.0
@@ -66,7 +72,6 @@ class data_manager:
                 "total_px":     total_px,
                 "coverage_pct": 0.0,
             }
-        
         return {
             "index":        index_name,
             "mean":         round(float(da.mean(skipna=True).values),3),
